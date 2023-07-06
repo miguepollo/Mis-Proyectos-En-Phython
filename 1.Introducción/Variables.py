0aee64186ee7e6f76e49e1acbd0505ac49b062fc
miguepollo <miguelgarciapel@protonmail.com>
2023-07-06 17:45:02 +0200

    añadir numeros con decimales en una variable flotante y sumarlos.

--- 1.Introducción/Variables.py
+++ 1.Introducción/Variables.py
@@ -1,7 +1,10 @@
 apellido = "garcia pelaéz"
 numero = 67+4
+flotante = 3.56+7.65
 
 print(apellido)
 print(type(apellido))
 print(numero)
 print(type(numero))
+print(flotante)
+print(type(flotante))

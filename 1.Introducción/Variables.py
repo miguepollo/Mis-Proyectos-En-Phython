ee7a6deb278c47a86df7196ba781504b8a49f47b
miguepollo <miguelgarciapel@protonmail.com>
2023-07-06 17:47:15 +0200

    Mover orden de llamado

--- 1.Introducción/Variables.py
+++ 1.Introducción/Variables.py
@@ -2,9 +2,9 @@ apellido = "garcia pelaéz"
 numero = 67+4
 flotante = 3.56+7.65
 
-print(apellido)
 print(type(apellido))
-print(numero)
+print(apellido)
 print(type(numero))
-print(flotante)
+print(numero)
 print(type(flotante))
+print(flotante)

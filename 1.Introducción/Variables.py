bd787208e8fb260ca8ad4abc6a72e2ceb884bcc8
miguepollo <miguelgarciapel@protonmail.com>
2023-07-06 18:07:16 +0200

    Añadir cosas graciosas. Y mas variables :(

--- 1.Introducción/Variables.py
+++ 1.Introducción/Variables.py
@@ -1,10 +1,16 @@
-apellido = "garcia pelaéz"
+nombre = "Ines"
+apellido = "Garcia Pelaéz"
 numero = 67+4
+delitospenales = 1000
 flotante = 3.56+7.65
 
-print(type(apellido))
-print(apellido)
+
 print(type(numero))
 print(numero)
 print(type(flotante))
 print(flotante)
+print(type(nombre))
+print(nombre)
+print(type(apellido))
+print(apellido)
+print( "Se califica como mata-hermanos EXPERTA ¡ALERTA HERMANIL!")

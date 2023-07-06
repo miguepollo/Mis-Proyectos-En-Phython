27c6706acebccec01c009ec4bc452190453b727e
miguepollo <miguelgarciapel@protonmail.com>
2023-07-06 19:12:49 +0200

    Añadir variable flotante.

--- 1.Introducción/variables-contantes.py
+++ 1.Introducción/variables-contantes.py
@@ -4,14 +4,33 @@ Personas = 89
 
 print(Personas)
 
-time.sleep(2)
+time.sleep(1.5)
 
 Personas = 67
 
 print(Personas)
 
-time.sleep(2)
+time.sleep(1.5)
 
 Personas = 140
 
 print(Personas)
+
+time.sleep(1)
+
+pi = 3.14
+
+print (pi)
+
+time.sleep(1)
+
+pi = 6.28
+
+print (pi)
+
+time.sleep(1)
+
+pi = 3.14
+
+print(pi)
+

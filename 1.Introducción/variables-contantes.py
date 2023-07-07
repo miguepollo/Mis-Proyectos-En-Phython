ddbac1e261d0c5dbb034ebcdb91aac971b2879b5
miguepollo <miguelgarciapel@protonmail.com>
2023-07-07 09:35:28 +0200

    desliarla:)

---
+++ 1.Introducción/variables-contantes.py
@@ -0,0 +1,43 @@
+import time
+
+Personas = 89
+
+print(Personas)
+
+time.sleep(1.5)
+
+Personas = 67
+
+print(Personas)
+
+time.sleep(1.5)
+
+Personas = 140
+
+print(Personas)
+
+time.sleep(1)
+
+pi = 3.14
+
+print (pi)
+
+time.sleep(1)
+
+pi = 6.28
+
+print (pi)
+
+time.sleep(1)
+
+pi = 3.14
+
+print(pi)
+
+textovariable = "hola"
+
+print(textovariable)
+
+time.sleep(1)
+
+textovariable = "adios"

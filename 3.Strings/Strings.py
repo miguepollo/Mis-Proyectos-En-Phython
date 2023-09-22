8b0ecdb622265eef919aa1be456b918bf3112ef6
miguepollo <miguelgarciapel@protonmail.com>
2023-09-22 11:44:12 +0200

    Añadir menos retraso

--- 3.Strings/Strings.py
+++ 3.Strings/Strings.py
@@ -4,31 +4,31 @@ cadenatexto = "Esto es un ejemplo de \"cadena\" de texto1"
 
 print(cadenatexto)
 
-time.sleep(3)
+time.sleep(1)
 
 cadenatexto2 = "Esto es un ejemplo de \ncadena  \nde texto2"
 
 print(cadenatexto2)
 
-time.sleep(3)
+time.sleep(1)
 
 cadenatexto3 = "Esto es un ejemplo de \tcadena  \tde texto3"# Nueva funcion que da dos tabuladores
 
 print(cadenatexto3)
 
-time.sleep(3)
+time.sleep(1)
 
 cadenatexto4 = "Esto es un ejemplo de \bcadena  \bde texto4"# Nueva funcion que quita los espacios
 
 print(cadenatexto4)
 
-time.sleep(3)
+time.sleep(1)
 
 cadenatexto5 = "Esto es un ejemplo de \fcadena  \fde texto5"# Nueva funcion que coloca un simbolo raro
 
 print(cadenatexto5)
 
-time.sleep(3)
+time.sleep(1)
 
 cadenatexto6 = "Esto es un ejemplo de cadena  \rde texto6"# Nueva funcion que elimina y cambia caracteres
 

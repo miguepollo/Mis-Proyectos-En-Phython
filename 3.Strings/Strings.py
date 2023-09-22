03e08c877dc2987f0c168310198fd4ffcfb992d1
miguepollo <miguelgarciapel@protonmail.com>
2023-09-22 11:28:08 +0200

    Añadir al final 2 o 3

--- 3.Strings/Strings.py
+++ 3.Strings/Strings.py
@@ -6,12 +6,12 @@ print(cadenatexto)
 
 time.sleep(3)
 
-cadenatexto2 = "Esto es un ejemplo de \ncadena  \nde texto"
+cadenatexto2 = "Esto es un ejemplo de \ncadena  \nde texto2"
 
 print(cadenatexto2)
 
 time.sleep(3)
 
-cadenatexto3 = "Esto es un ejemplo de \tcadena  \tde texto"# Nueva funcion que da dos tabuladores
+cadenatexto3 = "Esto es un ejemplo de \tcadena  \tde texto3"# Nueva funcion que da dos tabuladores
 
 print(cadenatexto3)

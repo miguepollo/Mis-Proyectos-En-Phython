da086cfa98f921d89695ac648fa824e232553952
miguepollo <miguelgarciapel@protonmail.com>
2023-09-22 11:26:51 +0200

    Añadir funcion con su retraso

--- 3.Strings/Strings.py
+++ 3.Strings/Strings.py
@@ -9,3 +9,9 @@ time.sleep(3)
 cadenatexto2 = "Esto es un ejemplo de \ncadena  \nde texto"
 
 print(cadenatexto2)
+
+time.sleep(3)
+
+cadenatexto3 = "Esto es un ejemplo de \tcadena  \tde texto"# Nueva funcion que da dos tabuladores
+
+print(cadenatexto3)

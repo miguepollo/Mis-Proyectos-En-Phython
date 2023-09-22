8e576cc8d90591a0ab702b86d5fd34d0d47700cd
miguepollo <miguelgarciapel@protonmail.com>
2023-09-22 11:16:11 +0200

    Añadir retraso y cadenatexto2 que es una funcion con lineas.

--- 3.Strings/Strings.py
+++ 3.Strings/Strings.py
@@ -3,3 +3,9 @@ import time
 cadenatexto = "Esto es un ejemplo de \"cadena\" de texto1"
 
 print(cadenatexto)
+
+time.sleep(5)
+
+cadenatexto2 = "Esto es un ejemplo de \ncadena  \nde texto"
+
+print(cadenatexto2)
